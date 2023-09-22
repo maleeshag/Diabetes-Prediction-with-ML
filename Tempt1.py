3dfb2ea5876914855715ad186184a53d69eb8147
maleeshagimmana <it21176456@my.sliit.lk>
2023-09-22 09:33:02 +0530

    Temp1.py file

---
+++ Tempt1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+# import pip
+# pip.main(["install","streamlit"])
+
+import streamlit as st
+from PIL import Image
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+
+# In[4]:
+
+
+st.header("Distance Detection App")
+
+
+# In[ ]:
+
+
+
+
